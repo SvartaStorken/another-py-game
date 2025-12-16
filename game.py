b0e6d4531533d95885510eb2b1d427ce270ef364
Martin Wallin <martin_junkan@hotmail.com>
2025-12-16 15:29:42 +0100

    in array and indentation fix

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import time
 
 bits_score = 0
 bits_array = [0, 0, 0]
-attempts = -1
+attempts = 0
 guest_questions = ["x", "x", "x"]
 the_person = "x"
 
@@ -31,53 +31,45 @@ print("***                   ***")
 time.sleep(1)
 
 while attempts < 3:
-    value = "false"
-    print("loop1")
+    while True:
+        random_person = random.randint(0, 2)
+        random_quote = random.randint(0, 2)
 
-    while attempts < 3:
-      print("loop2")
+        the_person = person[random_person]
+        if the_person in guest_questions:
+            continue
+        else:
+            break
 
-      random_person = random.randint(0, 2)
-      random_quote = random.randint(0, 2)
-    
+    if random_person == 0:
+      print("***  quote:           ***")
+      print(Richard_Stallman_quotes[random_quote])
+    elif random_person == 1:
+      print("***  quote:           ***")
+      print(Linus_Torvalds_quotes[random_quote])
+    elif random_person == 2:
+      print("***  quote:           ***")
+      print(Theo_de_Raadt_quotes[random_quote])
 
-      if random_person == 0:
-        print("***  quote:           ***")
-        print(Richard_Stallman_quotes[random_quote])
-      elif random_person == 1:
-        print("***  quote:           ***")
-        print(Linus_Torvalds_quotes[random_quote])
-      elif random_person == 2:
-        print("***  quote:           ***")
-        print(Theo_de_Raadt_quotes[random_quote])
+    print("guess which person it is:")
+    print("Firstname Lastname")
+    answer = input()
 
-      the_person = person[random_person]
-      print(the_person)
-      print(guest_questions[attempts])
+    guest_questions[attempts] = the_person
 
-      if the_person in guest_questions:
-        print("looping")
-      else:
-        break
+    if answer.lower() == the_person.lower():
+        print("***  Right answer!    ***")
+        print(the_person)
+        bits_array[attempts] = 1
 
-  print("guess which person it is:")
-  print("Firstname Lastname")
-  answer = input()
-  attempts += 1
-  guest_questions[attempts] = [the_person]
+    else:
+        print("***  Wrong answer     ***")
+        print(the_person)
 
-  if answer.lower() == the_person.lower():
-    print("***  Right answer!    ***")
-    print(the_person)
-    bits_array[attempts] = 1
-
-  else:
-    print("***  Wrong answer     ***")
-    print(the_person)
-
-  if attempts == 2:
-    break
+    attempts += 1
 time.sleep(1)
 
 print(*bits_array)
-print(*guest_questions)
+
+bit_calc = 1
+calc_array = [0, 0, 0]
